8b69b660f07664f47079860239f55e6b6a835e6c
twana koye <twana.00569020@gmail.com>
2024-08-29 15:44:23 +0300

    l

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
-//Data Fetching
-
-// 1. Server Side Rendering (SSR) :- the content is always up to date
-// 2. Static Site Generation (SSG) :- default of next.js
-// 3. Incremental Static ReGeneration (ISR) :- refreshing in interval time
-
 function Page() {
   return <div>page</div>;
 }
 export default Page;
+
+///////////Data Fetching//////////////////////////////////////////////////////
+
+// 1. Server Side Rendering (SSR) :- the content is always up to date
+// 2. Static Site Generation (SSG) :- default of next.js
+// 3. Incremental Static ReGeneration (ISR) :- refreshing in interval time
 
 // 1 :-
 // const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${1}`, {
@@ -24,3 +24,19 @@ export default Page;
 //   next: { revalidate: 10 },
 // });
 // const data = await res.json();
+
+//////////////We can define Metadata in two ways: Static and Dynamic.//
+
+// 1. Static Metadata
+// 2. Dynamic Metadata
+
+// 1 :-
+// export const metadata = {
+//   title: "Home",
+// };
+
+// 2 :-
+// export async function generateMetadata({ params, searchParams }) {
+//   const product = await getProduct(params.id);
+//   return { title: product.title };
+// }
--- app/posts/[postId]/page.js
+++ app/posts/[postId]/page.js
@@ -1,3 +1,7 @@
+export async function generateMetadata({ params, searchParams }) {
+  const product = await getProduct(params.id);
+  return { title: product.title };
+}
 export default function page({ params }) {
   const { postId } = params;
 
